7b6d53c56d29c9a94dc7b0b7b83dcf217e75b36a
Isak Kabir <isakkabir@users.noreply.github.com>
2020-03-19 20:51:30 +0100

    Gaussian Distribution Python Package Created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_19032020',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['dsnd_probability_19032020'],
+      author = 'Isak Kabir', 
+      author_email ='kabir.isak@gmail.com',
+      zip_safe=False)
+
